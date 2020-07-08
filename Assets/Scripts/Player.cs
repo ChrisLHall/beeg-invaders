e9b524295bc9021ffc456914dae8d9b8d2d35671
Chris Hall <mrchrislhall@gmail.com>
2020-07-08 01:01:02 -0700

    initial, added some assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        transform.position = pos;
+    }
+}
